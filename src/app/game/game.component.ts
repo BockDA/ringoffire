e15ef84b881f763d6beb84c68cf165b8c0553c0a
Bockinger <cbock2011@gmail.com>
2025-08-02 23:20:58 +0200

    bug in package fixed

--- src/app/game/game.component.ts
+++ src/app/game/game.component.ts
@@ -62,8 +62,7 @@ export class GameComponent implements OnInit {
 
   openDialog(): void {
     const dialogRef = this.dialog.open(DialogAddPlayerComponent);
-      
-    
+       
     dialogRef.afterClosed().subscribe((name: string ) => {
       if (name && name.length > 0) {
         this.game.players.push(name);
